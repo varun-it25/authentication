b47be9fce04959b416b1c7eef1c19092f386fab9
Varun kangotra <varunkangotra.it25@gmail.com>
2024-10-13 06:02:49 +0530

    DATABASE_STR Update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import fs from "fs";
 dotenv.config()
 
-export const DATABASE_STR = `mongodb://localhost:27017/auth`
-export const PORT = process.env.PORT
-export const secret_key = fs.readFileSync(`./private.pem`)
-export const public_key = fs.readFileSync(`./public.pem`)
+export const DATABASE_STR = process.env.DATABASE_STR;
+export const PORT = process.env.PORT;
+export const secret_key = fs.readFileSync(`./private.pem`);
+export const public_key = fs.readFileSync(`./public.pem`);
